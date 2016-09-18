cb753c00249db96e6cb04c030be978611c024de6
Andres Eloy <andreseloysv@gmail.com>
2016-09-18 18:46:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
   
   
-   app.get('/', function(req, res){
-     res.sendFile(__dirname + '/index.html');
-   });
+   //app.get('/', function(req, res){
+   //  res.sendFile(__dirname + '/index.html');
+   //});
  
    io.on('connection', function(socket){
      socket.on('chat message', function(msg){
        io.emit('chat message', msg);
      });
    });
+  io.on('connection', function(socket){
+	socket.on('beep', function(){
+		  socket.emit('boop');
+	  });
+  });
    http.listen(process.env.PORT || 5000);
 
