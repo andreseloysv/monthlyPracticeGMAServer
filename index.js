df63ff4a96bee1ea45fa6bdcf9df245987e0fb17
Andres Eloy <andreseloysv@gmail.com>
2016-09-18 17:25:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var express = require('express'),
- app = express(),
- server = require('http').createServer(app),
- io = require('socket.io').listen(server),
- io.set('transports', ['xhr-polling']);
- io.set('polling duration', 10);
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.configure(function () { 
+      io.set("transports", ["xhr-polling"]); 
+      io.set("polling duration", 10); 
+    });
   
- server.listen(process.env.PORT || 3000);
   
    app.get('/', function(req, res){
      res.sendFile(__dirname + '/index.html');
