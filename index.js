e844c21ed4ea2a2799503c84469dc91dd6ca2ef5
Andres Eloy <andreseloysv@gmail.com>
2016-09-18 19:05:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ io.on('connection', function(socket){
 		socket.emit('boopeeeee');
 	});
 })
-io.attach(.env.PORT || 5000);
+io.attach(process.env.PORT || 5000);
    //http.listen(process.env.PORT || 5000);
 
