466767e6773dbee74d8696ccb5e27ee15b79bdaa
Andres Eloy <andreseloysv@gmail.com>
2016-09-18 19:02:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')({
+  transports: ['websocket'],
+});
   
   
-   app.get('/', function(req, res){
+   //app.get('/', function(req, res){
    //  res.sendFile(__dirname + '/index.html');
-   	res.send(process.env.PORT);
-   });
+   //	res.send(process.env.PORT);
+   //});
  
    io.on('connection', function(socket){
      socket.on('chat message', function(msg){
