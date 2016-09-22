f82e3ec184529a5aa799f1c8cbb684f725837074
andrese <andreseloysv@gmail.com>
2016-09-22 19:52:31 +0200

    edasd
    asdsad

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ io.on('connection', function (socket)
     //socket.emit('newplayer');
     //socket.broadcast.emit('newplayer');
     socket.emit('connected');
-    socket.to('others').emit('newplayer');
+    //socket.to('others').emit('newplayer');
 //    socket.on('beep', function ()
 //    {
 //        socket.emit('boopeeeee');
