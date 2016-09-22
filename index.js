644fe60550f3b14f4596464a750ba525a710ae5d
andrese <andreseloysv@gmail.com>
2016-09-22 12:06:33 +0200

    Span new players
    Span new players

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')({
-  transports: ['websocket'],
+    transports: ['websocket'],
 });
-  
-  
-   //app.get('/', function(req, res){
-   //  res.sendFile(__dirname + '/index.html');
-   //	res.send(process.env.PORT);
-   //});
- 
-io.on('connection', function(socket){
-	socket.emit('boop');
-	socket.on('beep', function(){
-		socket.emit('boopeeeee');
-	});
-	socket.on('position', function(msg){
-		io.emit('position', msg);
-	});
-})
+
+
+//app.get('/', function(req, res){
+//  res.sendFile(__dirname + '/index.html');
+//	res.send(process.env.PORT);
+//});
+
+io.on('connection', function (socket)
+{
+    socket.emit('newplayer');
+//    socket.on('beep', function ()
+//    {
+//        socket.emit('boopeeeee');
+//    });
+    socket.on('position', function (msg)
+    {
+        io.emit('position', msg);
+    });
+});
 io.attach(process.env.PORT || 5000);
-   //http.listen(process.env.PORT || 5000);
+//http.listen(process.env.PORT || 5000);
 
