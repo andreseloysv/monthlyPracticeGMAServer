76258a745f9cc2ced226e6f95b8764b67bf08e77
andrese <andreseloysv@gmail.com>
2016-09-24 12:28:02 +0200

    kalsd
    jkljkl

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ io.on('connection', function (socket)
     socket.on('createroom', function (msg)
     {
         var roomId = String(new Date().getTime());
-        roomList.push(new room(roomId, msg.roomName, [msg.playerid]));
+        roomList.push(new room(roomId, msg.roomname, [msg.playerid]));
         socket.emit('roomid', {roomid: roomId});
     });
 
@@ -66,6 +66,8 @@ io.on('connection', function (socket)
 io.attach(process.env.PORT || 5000);
 //http.listen(process.env.PORT || 5000);
 class room {
+    roomId;
+    roomName;
     userList = [];
     constructor(roomId, roomName, userList) {
         this.roomId = roomId;
