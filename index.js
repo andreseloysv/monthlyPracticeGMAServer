99126341cd2770cd56e2f1de6104e38dc589e43e
Andres Eloy <andreseloysv@gmail.com>
2017-08-19 15:58:24 +0200

    arreglo del la funcion getlocations

--- index.js
+++ index.js
@@ -115,10 +115,7 @@ io.on('connection', function (socket)
 
     socket.on('getLocations', function (msg)
     {
-        var newPlayerId = String(new Date().getTime());
-        socket.emit('playeradded', {playerid: newPlayerId});
-        socket.broadcast.emit('newplayer', {playerid: newPlayerId});
-        //socket.emit('roomlist', JSON.stringify(roomList));
+        socket.emit('locations', {locations: getLocations()});
     });
 
 });
