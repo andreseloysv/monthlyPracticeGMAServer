cdc67467ab3e87015c25e4eb54aef84dcce6e171
Andres Eloy <andreseloysv@gmail.com>
2017-08-19 16:02:09 +0200

    move function position

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 "use strict";
+
+//Test postgres conection:
+function getLocations(){
+    var result = "";
+    var pg = require('pg');
+    var conString = "postgres://wqqmkpkvddqxrf:d65bdb63cb9f3de3796198b42a27ae7ccf1b0e65864832f08b9cc23c7b51d0aa@ec2-46-137-97-169.eu-west-1.compute.amazonaws.com:5432/da4514sv0048rq";
+    
+    var client = new pg.Client(conString);
+    client.connect();
+    
+    var query = client.query("SELECT * FROM location");
+    //fired after last row is emitted
+    
+    query.on('row', function(row) {
+        result = result+row.toString();
+        console.log(row);
+    });
+    
+    query.on('end', function() {
+        client.end();
+    });
+    return result;
+}
+
 class room {
     constructor(roomId, roomName, userList) {
         this.roomId = roomId;
@@ -122,25 +146,4 @@ io.on('connection', function (socket)
 io.attach(process.env.PORT || 5000);
 //http.listen(process.env.PORT || 5000);
 
-//Test postgres conection:
-function getLocations(){
-    var result = "";
-    var pg = require('pg');
-    var conString = "postgres://wqqmkpkvddqxrf:d65bdb63cb9f3de3796198b42a27ae7ccf1b0e65864832f08b9cc23c7b51d0aa@ec2-46-137-97-169.eu-west-1.compute.amazonaws.com:5432/da4514sv0048rq";
-    
-    var client = new pg.Client(conString);
-    client.connect();
-    
-    var query = client.query("SELECT * FROM location");
-    //fired after last row is emitted
-    
-    query.on('row', function(row) {
-        result = result+row.toString();
-        console.log(row);
-    });
-    
-    query.on('end', function() {
-        client.end();
-    });
-    return result;
-}
+
